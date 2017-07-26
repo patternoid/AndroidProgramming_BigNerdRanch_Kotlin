5b605df8471b6df1e7d263bdb9f8237db7be6ddf
Patternoid <patternoid@Patternoidui-MacBook-Air.local>
2017-07-26 20:52:59 +0900

    * Chapter 3장 코드
    -챌린지 적용하기 전

---
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.geoquiz
+
+/**
+ * Created by patternoid on 2017. 7. 25..
+ */
+data class Question( val textResId : Int, val answerTrue : Boolean)
--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizKtActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizKtActivity.kt
@@ -2,7 +2,6 @@ package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_quiz.*
 
@@ -13,18 +12,58 @@ import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizKtActivity : AppCompatActivity()
 {
+    private var mQuestionBank : Array<Question> = arrayOf(
+            Question(R.string.question_oceans,true),
+            Question(R.string.question_africa,false),
+            Question(R.string.question_americas,false),
+            Question(R.string.question_asia,false),
+            Question(R.string.question_mideast,false)
+    )
+
+    private var mCurrentIndex : Int = 0;
+
+
     override fun onCreate( savedInstanceState : Bundle? )
     {
         super.onCreate( savedInstanceState )
         setContentView(R.layout.activity_quiz)
 
         true_button!!.setOnClickListener { v ->
-            Toast.makeText(this@QuizKtActivity, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(true)
         }
 
         false_button!!.setOnClickListener { v ->
-            Toast.makeText(this@QuizKtActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
-
+            checkAnswer(false)
         }
+
+        next_button!!.setOnClickListener { v ->
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+
+
+    private fun updateQuestion(){
+        val question :Int = mQuestionBank[mCurrentIndex].textResId
+        question_text_view!!.setText(question)
+    }
+
+
+
+    private fun checkAnswer( userPressedTrue : Boolean ){
+
+        val ansewerIsTrue   = mQuestionBank[mCurrentIndex].answerTrue
+        var messageResId    = 0
+
+        if( userPressedTrue == ansewerIsTrue ){
+            messageResId = R.string.correct_toast
+        } else {
+            messageResId = R.string.incorrect_toast
+        }
+
+        Toast.makeText(this@QuizKtActivity, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
