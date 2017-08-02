bdf363d12b93d199329ad59545b432ba656c318e
Patternoid <patternoid@Patternoidui-MacBook-Air.local>
2017-08-02 20:57:16 +0900

    CriminalIntent 프로젝트 추가

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CrimeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crime)
+    }
+}
